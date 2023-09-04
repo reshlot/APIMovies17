66dd75cb58d4cea2dc5ba2273546c327da19e581
HP <reshmaksriet@gmail.com>
2023-09-04 15:44:58 +0530

    Initial commit

---
+++ APIMoviesAss17/APIMoviesAss17/Data/MoviesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APIMoviesAss17.Model;
+
+namespace APIMoviesAss17.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext (DbContextOptions<MoviesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APIMoviesAss17.Model.Movie> Movie { get; set; } = default!;
+    }
+}
